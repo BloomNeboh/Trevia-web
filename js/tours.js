91b9fd649c70b7ac35ae4674c885f09b5151e867
BloomNE9Z <ellymchome21@gmail.com>
2025-09-26 10:01:02 +0300

    Create tours.js

---
+++ js/tours.js
@@ -0,0 +1,20 @@
+const tours = [
+  {
+    name: "Kilimanjaro Hike",
+    img: "images/kilimanjaro.jpg",
+    description: "Climb Africa's highest peak with experienced guides.",
+    price: "$1200 / person"
+  },
+  {
+    name: "Serengeti Safari",
+    img: "images/serengeti.jpg",
+    description: "Witness the great migration and big cats in action.",
+    price: "$1500 / person"
+  },
+  {
+    name: "Zanzibar Beach",
+    img: "images/zanzibar.jpg",
+    description: "Relax on turquoise beaches and explore spice farms.",
+    price: "$900 / person"
+  }
+];
